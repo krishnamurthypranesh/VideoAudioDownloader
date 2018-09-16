b120609261b89cdc59fd2fe7b0a3b81032834679
krishnamurthypranesh <pkstockac@gmail.com>
2018-09-16 07:25:57 +0530

    Added
    a way to select particular videos to download

--- downloadVideo.py
+++ downloadVideo.py
@@ -1,35 +1,38 @@
 # import modules================================================================
 import bs4
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 import time
 import datetime
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 # get the links of the videos that need to be downloaded========================
 
 # get input html file from the user
 html_file_directory = input("Enter the path to the html file: ")
+
 # parse page
 contentSoup = bs4.BeautifulSoup(open(html_file_directory), "html.parser")
 
 # getting the links of the videos from the playlist
-videos = contentSoup.find_all(
+video_links = contentSoup.find_all(
             "a",
             class_= "yt-simple-endpoint style-scope ytd-playlist-video-renderer"
             )
 
 # extracting the links from the videos
-links = [i.get("href") for i in videos]
+links = [i.get("href") for i in video_links]
+
+#get videos that need to be downloaded from user
+print("There are " + str(len(links)) + " videos. Which need to be downloaded? ")
+videos_to_download = list(map(int, input().split()))
 
 # download the required files===================================================
-browser = webdriver.Chrome(executable_path = "./chromedriver")
-
 # function to download the file-------------------------------------------------
-def downloadVideo(url): # function to download videos
-
+browser = webdriver.Chrome(executable_path = "./chromedriver")
+def convertToAudio(url):
     # site to download audio from
     browser.get("https://www.onlinevideoconverter.com/mp3-converter")
-    browser.set_window_size(900, 900) # setting the browser window size
-    browser.set_window_position(0, 0) # setting the browser position window
+    browser.set_window_size(900, 900)
+    browser.set_window_position(0, 0)
     # pasting url to convert
     browser.find_element_by_id("texturl").send_keys(url)
     time.sleep(5)
@@ -48,14 +51,13 @@ def downloadVideo(url): # function to download videos
 
 # downloading videos in links---------------------------------------------------
 try:
-    for i in range(len(links)):
-        print("Downloaded video " + str(i + 1)) # message to indicate video download
-        downloadVideo(links[i])
-except Exception as err: # catching errors
+    for i in range(videos_to_download[0], videos_to_download[1]):
+        convertToAudio(links[i])
+        print("Downloaded video " + str(i + 1))
+except Exception as err: # exception handling
     print("An error occured! Error Code: \n" + str(err))
     print("Closing the browser! and terminating all operations")
-    try: # writing errors raised to file
-        # appending to file
+    try:
         with open("errors.txt", "a") as errors:
             current_time = str(datetime.datetime.now())
             print("The following error occured while downloading video " +
